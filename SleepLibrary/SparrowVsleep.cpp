20d19eab256e36bda6959751a5e267493b156798
Theodor Ungureanu <theodor_ungureanu@Theodor-MacBook-Pro.local>
2018-08-09 15:21:23 +0300

    Added full working sleep library. Working for sleep >= 1 second

--- SleepLibrary/SparrowVsleep.cpp
+++ SleepLibrary/SparrowVsleep.cpp
@@ -5,30 +5,24 @@
 #include <util/delay.h>
 #include "SparrowVsleep.h"
 
+// #define DEBUG // uncomment for debunging
+			  // (also comment /* (1 << PRUSART0) */ from PRR0 )
+
 int global_seconds;
+bool global_data_retention;
 
 ISR(WDT_vect)
 {
-	Serial.println("Watchdog Interrupt ");
-	Serial.flush();
+	#ifdef DEBUG
+		Serial.println("Watchdog Interrupt ");
+		Serial.flush();
 
-	Serial.print("global_seconds: ");
-	Serial.println(global_seconds);
-	Serial.flush();
+		Serial.print("global_seconds: ");
+		Serial.println(global_seconds);
+		Serial.flush();
+	#endif
 
-	// // reenable the transciever
-	// PRR1 &= ~(1 << PRTRX24);
-	//
-	// // reenable ADC
-	// ADCSRA = (1 << ADEN);
-	//
-	// //restore PRR
-	// PRR0 = 0;
-	// PRR1 = 0;
-	//
-	// wdt_disable();
-
-	if (global_seconds <= 0)
+	if (global_seconds == 0)
 	{
 		Serial.println();
 		Serial.println("Ready sleep");
@@ -46,39 +40,57 @@ ISR(WDT_vect)
 	}
 	else
 	{
+	    #ifdef DEBUG
+			Serial.println();
+			Serial.println("going to sleep again");
+		#endif
+
 		// going to sleep again
-		Serial.println();
-		Serial.println("going to sleep again");
 		choose(global_seconds);
 	}
 }
 
 void choose(int seconds)
 {
-	Serial.println("choose");
-	Serial.flush();
-
-	if(seconds % 8 == 0) {
-		Serial.println("choose 8");
+	#ifdef DEBUG
+		Serial.println("entering choose function");
 		Serial.flush();
+	#endif
+
+	if(seconds % 8 == 0)
+	{
+		#ifdef DEBUG
+			Serial.println("choose 8");
+			Serial.flush();
+		#endif
 		setup_WDT(8);
 	}
-	else if (seconds % 4 == 0) {
-		Serial.println("choose 4");
-		Serial.flush();
+	else if (seconds % 4 == 0)
+	{
+		#ifdef DEBUG
+			Serial.println("choose 4");
+			Serial.flush();
+		#endif
 		setup_WDT(4);
 	}
-	else if (seconds % 2 == 0) {
-		Serial.println("choose 2");
-		Serial.flush();
+	else if (seconds % 2 == 0)
+	{
+		#ifdef DEBUG
+			Serial.println("choose 2");
+			Serial.flush();
+		#endif
 		setup_WDT(2);
 	}
-	else if (seconds % 1 == 0) {
-		Serial.println("choose 1");
-		Serial.flush();
+	else if (seconds % 1 == 0)
+	{
+		#ifdef DEBUG
+			Serial.println("choose 1");
+			Serial.flush();
+		#endif
 		setup_WDT(1);
 	}
-	else {
+	else
+	{
 		//TODO for less than 1 second
 		return;
 	}
@@ -86,8 +98,10 @@ void choose(int seconds)
 
 void setup_WDT(int situation)
 {
-	Serial.println("setup WDT");
-	Serial.flush();
+	#ifdef DEBUG
+		Serial.println("setup WDT");
+		Serial.flush();
+	#endif
 
 	cli();
 
@@ -95,7 +109,6 @@ void setup_WDT(int situation)
 	MCUSR &= ~(1 << WDRF); // Clear the reset flag. (because the data sheet said to)
 
 	// Setup Watchdog for interrupt and not reset, and a approximately x seconds timeout
-
 	switch (situation)
 	{
 		// wake up after 8 seconds
@@ -103,8 +116,10 @@ void setup_WDT(int situation)
 				WDTCSR = (1 << WDCE) | (1 << WDE);
 				WDTCSR = (1 << WDIE) | (1 << WDP3) | (1 << WDP0);
 				global_seconds -= 8;
-				Serial.println("WDT 8");
-				Serial.flush();
+				#ifdef DEBUG
+					Serial.println("WDT 8");
+					Serial.flush();
+				#endif
 				break;
 
 		// wake up after 4 seconds
@@ -112,8 +127,10 @@ void setup_WDT(int situation)
 				WDTCSR = (1 << WDCE) | (1 << WDE);
 				WDTCSR = (1 << WDIE) | (1 << WDP3);
 				global_seconds -= 4;
-				Serial.println("WDT 4");
-				Serial.flush();
+				#ifdef DEBUG
+					Serial.println("WDT 4");
+					Serial.flush();
+				#endif
 				break;
 
 		// wake up after 2 seconds
@@ -121,8 +138,10 @@ void setup_WDT(int situation)
 				WDTCSR = (1 << WDCE) | (1 << WDE);
 				WDTCSR = (1 << WDIE) | (1 << WDP2) | (1 << WDP1) | (1 << WDP0);
 				global_seconds -= 2;
-				Serial.println("WDT 2");
-				Serial.flush();
+				#ifdef DEBUG
+					Serial.println("WDT 2");
+					Serial.flush();
+				#endif
 				break;
 
 		// wake up after 1 second
@@ -130,21 +149,28 @@ void setup_WDT(int situation)
 				WDTCSR = (1 << WDCE) | (1 << WDE);
 				WDTCSR = (1 << WDIE) | (1 << WDP2) | (1 << WDP1);
 				global_seconds -= 1;
-				Serial.println("WDT 1");
-				Serial.flush();
+				#ifdef DEBUG
+					Serial.println("WDT 1");
+					Serial.flush();
+				#endif
 				break;
 
 		//TODO for less than 1 second
 	}
 	sei();
-	SparrowV_SleepSet(true);
+
+	// sleep setup and going to sleep
+	SparrowV_SleepSet(global_data_retention);
 }
 
 void SparrowV_SleepSet(bool data_retention)
 {
+	#ifdef DEBUG
+		Serial.println("sleep setup");
+		Serial.flush();
+	#endif
+
 	// prepare Deep-sleep Mode
-	Serial.println("sleep setup");
-	Serial.flush();
 
 	// transcieverul off
 	PRR1 |= (1 << PRTRX24);
@@ -154,7 +180,7 @@ void SparrowV_SleepSet(bool data_retention)
 
 	// Power reduction registers
 	PRR0 |= (1 << PRTWI) | (1 << PRTIM2) | (1 << PRTIM0) | (1 << PRPGA)
-	    | (1 << PRTIM1) | (1 << PRSPI) | (1 << PRADC)  /*| (1 << PRUSART0)*/;
+	    | (1 << PRTIM1) | (1 << PRSPI) | (1 << PRADC) | (1 << PRUSART0);
 
 	PRR1 |= (1 << PRTIM5) | (1 << PRTIM4) | (1 << PRTIM3) | (1 << PRUSART1)
 			| (1 << PRUSART1);
@@ -173,19 +199,15 @@ void SparrowV_SleepSet(bool data_retention)
 
 void SparrowV_SleepInit(int seconds, bool data_retention)
 {
-	// 8 < seconds
+	// update global variables
 	global_seconds = seconds;
-	Serial.println("global_seconds: ");
-	Serial.println(global_seconds);
-	Serial.flush();
+	global_data_retention = data_retention;
+
+	#ifdef DEBUG
+		Serial.println("global_seconds: ");
+		Serial.println(global_seconds);
+		Serial.flush();
+	#endif
 
 	choose(seconds);
-	// setup_WDT(8);
-
-	//TODO
-	// 4 < second < 8
-	// 2 < seconds < 4
-	// 1 < secons < 2
-
-	// SparrowV_SleepSet(data_retention);
 }
