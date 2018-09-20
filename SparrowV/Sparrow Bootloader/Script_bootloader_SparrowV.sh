1bf5da0174886f8464e99abd00b49fe218a2424c
Theodor Ungureanu <theodor_ungureanu@theodor-mbp.home>
2018-09-20 19:39:38 +0300

    Removed

--- SparrowV/Sparrow Bootloader/Script_bootloader_SparrowV.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-avrdude -c avrispmkii -p m644rfr2 -e -u -U flash:w:ATmegaBOOT_168_atmega644rfr2.hex
-avrdude -c avrispmkii -P usb -p m644rfr2 -U lfuse:w:0xf7:m
-avrdude -c avrispmkii -P usb -p m644rfr2 -U hfuse:w:0xd8:m
-avrdude -c avrispmkii -P usb -p m644rfr2 -U efuse:w:0xfd:m
